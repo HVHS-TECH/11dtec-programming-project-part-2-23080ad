34cbdc997a06c19ede8b0eca7c1e69885db82e0a
23080ad <23080ad@hvhs.school.nz>
2025-06-06 03:12:40 +0000

    done

--- homePage.js
+++ homePage.js
@@ -3,7 +3,9 @@
 //Variables
 let seatsPerCar = ["so you dont want a car?", "May I suggest the BAC Mono?", "May I suggest the MX5?", "We have no three seated cars sorry",
     "May I suggest the Mini?", "May I suggest the Falcon?", "We have no six seated cars sorry", "May I suggest the Outlander?", "May I suggest the Estima?", "One moment please."];
-var seatsInCarCounter = 0;
+var seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const MINSEATS = 0;
+const MAXSEATS = 8;
 var carReselect;
 var viewerName;
 var userSeats;
@@ -21,27 +23,25 @@ function askViewerName() {
     testName = viewerName;
 }
 
-/*************************************************************************************
-This asks the user how many seats they would like in their car,
-and then reads the array to see what car is currently avaliable with that many seats.
-*************************************************************************************/
+
+//This asks the user how many seats they would like in their car, and then reads the array to see what car is currently avaliable with that many seats.
+
 function askUserSeats() {
     userSeats = prompt("hello " + testName + ", please enter how many seats you would like in your car" +
         "\nso that we can try and find a model that matches your reqirements?");
-    alert(seatsPerCar[userSeats]);
-    seatsInCarCounter = userSeats;
-    if (userSeats >= 9) {
-        alert("We do not have any cars with 9 or more seats sorry. Goodbye.")
-    } else if (userSeats == 0) {
-        alert("Goodbye.")
-    } else if (userSeats == 6) {
-        alert("its over for you")
-    } else if (userSeats == 3) {
-        alert("its over for you")
+    if (userSeats == "" || userSeats == " " || userSeats == null || userSeats < MINSEATS || userSeats > MAXSEATS || isNaN(userSeats)) {
+        alert("invalid! \nYour input wasnt a number.");
+        userSeats = askUserSeats();
     } else {
+        alert(seatsPerCar[userSeats]);
+        seatsInCarCounter = userSeats
+        carReselect = prompt("does this car interest you?")
         carReselect = askCarReselect();
     }
 }
+
+
+
 
 
 /***************************************************************************************
@@ -50,23 +50,25 @@ If they do not like their car it asks them if they would likr to look at another
 if they still say no it directs them off the site.
 ***************************************************************************************/
 function askCarReselect() {
-    carReselect = prompt("does this car interest you?")
-
-    if (carReselect == "yes") {
-        alert("Perfect")
-    } else if (carReselect == "no") {
-        reselectConfirm = prompt("Sorry, we dont have any more cars with " + seatsInCarCounter + " seats. \nWould you like to rent a different car?");
-        if (reselectConfirm == "no") {
-            alert("thank you for shopping at Robs Rentals, we will notify you via email when we restock our " + seatsInCarCounter + " seater range.")
-        } else if (reselectConfirm == "yes") {
-            userSeats = askUserSeats();
-        } else {
-            alert("I really dont understand sorry.")
-        }
+    if (userSeats == 3 || userSeats == 6) {
+        rentNewCar = prompt("would you like to rent another car")
     } else {
-        alert("What are you saying?")
-    }
+        if (carReselect == "yes") {
+            alert("Perfect")
+        } else if (carReselect == "no") {
+            reselectConfirm = prompt("Sorry, we dont have any more cars with " + seatsInCarCounter + " seats. \nWould you like to rent a different car?");
+            if (reselectConfirm == "no") {
+                alert("thank you for shopping at Robs Rentals, we will notify you via email when we restock our " + seatsInCarCounter + " seater range.")
+            } else if (reselectConfirm == "yes") {
+                userSeats = askUserSeats();
+            } else {
+                alert("I really dont understand sorry.")
+            }
+        } else {
+            alert("What are you saying?")
 
+        }
+    }
 
 }
 
