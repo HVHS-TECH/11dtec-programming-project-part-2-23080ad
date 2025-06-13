8ffd2a6f2030c47960ede64e305e9793db1644e9
23080ad <23080ad@hvhs.school.nz>
2025-06-13 03:14:57 +0000

    fix it

--- htmleffects.js
+++ htmleffects.js
@@ -1,5 +1,13 @@
-dropDownS = seatsDropDown();
-function seatsDropDown(){
-    for (i>1 || )
+var seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+var dropDown = {
+    value: "angus",
+    type: "text"
+}
+
+function seatsDropDown() {
+    for (i = 0; i < seatsInCarCounter.length; i++) {
+        seatsInCar = seatsInCarCounter[i]
+    };
 
 }
