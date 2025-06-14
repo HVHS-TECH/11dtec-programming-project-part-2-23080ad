5219f2be7bbfd77d6ba847946c3e574047845576
23080ad <23080ad@hvhs.school.nz>
2025-06-14 05:34:22 +0000

    next page

--- htmleffects.js
+++ htmleffects.js
@@ -3,7 +3,7 @@ let seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 function seatsDropDown() {
     for (i = 0; i < seatsInCarCounter.length; i++) {
         var newOption = '<option value="' + seatsInCarCounter[i] + '>"' + seatsInCarCounter[i] + '</option>';
-        fruitselect += newOption;
-    };
+        fruitselect.innerHTML += newOption;
+    }
 
 }
