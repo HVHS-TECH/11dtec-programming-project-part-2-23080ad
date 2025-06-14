94510c77b75f68f300bf91459630d60e133b1f80
23080ad <23080ad@hvhs.school.nz>
2025-06-14 05:04:06 +0000

    dropdown

--- htmleffects.js
+++ htmleffects.js
@@ -1,13 +1,9 @@
-var seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-var dropDown = {
-    value: "angus",
-    type: "text"
-}
+let seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 function seatsDropDown() {
     for (i = 0; i < seatsInCarCounter.length; i++) {
-        seatsInCar = seatsInCarCounter[i]
+        var newOption = '<option value="' + seatsInCarCounter[i] + '>"' + seatsInCarCounter[i] + '</option>';
+        fruitselect += newOption;
     };
 
 }
