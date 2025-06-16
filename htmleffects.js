88b484b70363d6f77ed6320a22442a3dd2bee348
23080ad <23080ad@hvhs.school.nz>
2025-06-16 21:39:45 +0000

    save

--- htmleffects.js
+++ htmleffects.js
@@ -1,5 +1,5 @@
 let seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-var carPicked = 5;
+var carPicked;
 function seatsDropDown() {
     for (i = 0; i < seatsInCarCounter.length; i++) {
         var newOption = '<option value="' + seatsInCarCounter[i] + '>"' + seatsInCarCounter[i] + '</option>';
@@ -7,9 +7,7 @@ function seatsDropDown() {
     }
     var carPicked = seatsInCarCounter[i];
 }
-console.log("htmleffects");
-console.log(carPicked);
 
-function carFromSeats() {
-carResult.innerHTML= carPicked;
+function confirmCarSeats() {
+    carResult.innerHTML += "testB";
 }
