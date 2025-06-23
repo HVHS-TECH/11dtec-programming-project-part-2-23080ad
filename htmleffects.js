b34c05245e96025fe8df605ea538a45ac778420a
23080ad <23080ad@hvhs.school.nz>
2025-06-23 08:48:21 +0000

    changes

--- htmleffects.js
+++ htmleffects.js
@@ -1,15 +1,21 @@
+console.log(htmleffects.js)
 let seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 function seatsDropDown() {
-    for (i = 0; i < seatsInCarCounter.length; i++) {
-        var newOption = '<option value="' + seatsInCarCounter[i] + '>"' + seatsInCarCounter[i] + '</option>';
+    let seatPicker = document.getElementById('seatPicker');
+    for (let i = 0; i < seatsInCarCounter.length; i++) {
+        var newOption = '<option value="' + seatsInCarCounter[i] + '">' + seatsInCarCounter[i] + '</option>';
         seatPicker.innerHTML += newOption;
     }
-
 }
+
 
 function confirmCarSeats(){
-    var message = "Number of seats: ";
-    message = message + seatsInCarCounter[i];
+    var seatPicker = document.getElementById('seatPicker');
+    var carResult = document.getElementById('carResult');
+    var message = "Number of seats: " + seatPicker.value;
     carResult.innerHTML = message;
+    console.log(message);
 }
+
+//window.onload = seatsDropDown;
