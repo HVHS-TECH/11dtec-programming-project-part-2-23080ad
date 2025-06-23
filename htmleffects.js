8291c0dc1d58a297c7ccb66daf8401b4f7c90ab8
23080ad <23080ad@hvhs.school.nz>
2025-06-23 11:16:37 +0000

    eventlistener

--- htmleffects.js
+++ htmleffects.js
@@ -1,17 +1,35 @@
-const seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const seatPicker = document.getElementById('seatPicker');
-const carResult = document.getElementById('carResult');
-
+const allCars = ["place holder", "BAC Mono", "MX5", "place holder", "Mini", "Falcon", "place holder", "Outlander", "Estima"];
+/** 
+This function runs a loop which sorts through an array
+and makes each individual value of it an option for a select tag using innerHTML.
+**/
 function seatsDropDown() {
+    let seatsInCarCounter = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     for (let i = 0; i < seatsInCarCounter.length; i++) {
         var newOption = '<option value="' + seatsInCarCounter[i] + '">' + seatsInCarCounter[i] + '</option>';
         seatPicker.innerHTML += newOption;
     }
 }
 
-function confirmCarSeats(){
-    var message = "Number of seats: " + seatPicker.value;
-    carResult.innerHTML = message;
+/**
+This function reads the value selected on the carSeats dropdown and apends that to the message variable.
+This value is then displayed through a paragraph tag using innerHTML.
+**/
+
+
+function updateCarDetails() {
+    let carResult = document.getElementById('carResult');
+    carResult.innerHTML = "Number of seats: " + seatPicker.value;
+
+    let carModel = document.getElementById('carModel');
+    carModel.innerHTML = "Car model: " + allCars[seatPicker.value];
 }
 
-window.onload = seatsDropDown;
+seatsDropDown();
+
+// when the selection is changed this updates its details
+seatPicker.addEventListener('change', updateCarDetails);
+
+// Also update details on page load (if we want values shown on first load then uncomment this)
+//updateCarDetails();
