4d03af0efa9df3288f077752aa0e5f063c472b2e
23080ad <23080ad@hvhs.school.nz>
2025-07-06 03:41:17 +0000

    final touches

--- htmleffects.js
+++ htmleffects.js
@@ -1,6 +1,7 @@
 const seatPicker = document.getElementById('seatPicker');
 var carSelect;
-
+var rentTotal;
+var daysRented;
 const allCars = ["There are no 0 seated cars in existence", "BAC Mono", "MX5", "i30", "Mini", "Falcon", "Prado", "Outlander", "Estima"];
 /** 
 This function runs a loop which sorts through an array
@@ -53,15 +54,15 @@ to the value entered in the "days rented" form.
 function rentInput() {
     const minDays = 1;
     const maxDays = 31;
-    var daysRented = document.getElementById('daysRented');
+    daysRented = document.getElementById('daysRented');
     var daysRentedMessage = document.getElementById('daysRentedMessage');
     var rentFor = "Renting for: ";
     if (daysRented.value == "" || daysRented.value == " " || daysRented.value > maxDays) {
         rentFor = "Invalid input! <br>Input must be a number between 1 and 31!";
         daysRented.value = 31;
         daysRentedMessage.innerHTML = rentFor;
-    } else if (daysRented.value == "" || daysRented.value == " " || daysRented.value < minDays ) {
-         rentFor = "Invalid input! <br>Input must be a number between 1 and 31!";
+    } else if (daysRented.value == "" || daysRented.value == " " || daysRented.value < minDays) {
+        rentFor = "Invalid input! <br>Input must be a number between 1 and 31!";
         daysRented.value = 1;
         daysRentedMessage.innerHTML = rentFor;
     } else {
@@ -73,7 +74,7 @@ function rentInput() {
 
 //Bac-mono image javascript
 var img = document.createElement('img');
-img.src = 'BAC-mono.png';
+img.src = 'images/BAC-mono.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image1';
@@ -82,7 +83,7 @@ document.getElementById('BAC-mono').appendChild(img);
 
 //MX5 image javascript
 var img = document.createElement('img');
-img.src = 'MX5.png';
+img.src = 'images/MX5.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image2';
@@ -91,7 +92,7 @@ document.getElementById('MX5').appendChild(img);
 
 //i30 image javascript
 var img = document.createElement('img');
-img.src = 'i30.png';
+img.src = 'images/i30.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image3';
@@ -100,7 +101,7 @@ document.getElementById('i30').appendChild(img);
 
 //MINI image javascript        
 var img = document.createElement('img');
-img.src = 'MINI.png';
+img.src = 'images/MINI.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image4';
@@ -109,7 +110,7 @@ document.getElementById('MINI').appendChild(img);
 
 //Falcon image javascript
 var img = document.createElement('img');
-img.src = 'Falcon.png';
+img.src = 'images/Falcon.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image5';
@@ -118,7 +119,7 @@ document.getElementById('Falcon').appendChild(img);
 
 //Prado image javascript
 var img = document.createElement('img');
-img.src = 'Prado.png';
+img.src = 'images/Prado.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image6';
@@ -127,7 +128,7 @@ document.getElementById('Prado').appendChild(img);
 
 //Outlander image javascript
 var img = document.createElement('img');
-img.src = 'Outlander.png';
+img.src = 'images/Outlander.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image7';
@@ -136,7 +137,7 @@ document.getElementById('Outlander').appendChild(img);
 
 //Estima image javascript        
 var img = document.createElement('img');
-img.src = 'Estima.png';
+img.src = 'images/Estima.png';
 img.alt = 'Placeholder image';
 img.className = 'my-image';
 img.id = 'image8';
@@ -145,67 +146,81 @@ document.getElementById('Estima').appendChild(img);
 
 //These functions update id "selectedCar" so that it displays what car teh user has selected.
 function carOutPut1() {
-    let carSelect = "BACmono";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "BAC Mono";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
+
 
 function carOutPut2() {
-    let carSelect = "MX5";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "MX5";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
 
+
 function carOutPut3() {
-    let carSelect = "i30";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "i30";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
 
 function carOutPut4() {
-    let carSelect = "Mini";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "Mini";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
+
+
 function carOutPut5() {
-    let carSelect = "Falcon";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "Falcon";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
+
 
 function carOutPut6() {
-    let carSelect = "Prado";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "Prado";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
+
 
 function carOutPut7() {
-    let carSelect = "Outlander";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "Outlander";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
+
 
 function carOutPut8() {
-    let carSelect = "Estima";
-    let checkOut = document.getElementById('checkOut');
-    checkOut = "You have selected the " + carSelect + ".";
-    selectedCar.innerHTML = checkOut;
+    carSelect = "Estima";
+    let message = "You have selected the " + carSelect + ".";
+    document.getElementById('selectedCar').innerHTML = message;
 }
-var rentTotal = 0;
 
-function checkOut(){
-    
-    let rentTotal = daysRented.value * 10;
-    var userReceipt = "Youre final cost totals at: $" + rentTotal;
+var insVal = 0;
+function insUpdate() {
+    var checkBox = document.getElementById("insuranceBox");
+    var insResult = document.getElementById("insResult");
+    if (checkBox.checked == true) {
+        insResult.style.display = "block";
+        insVal = 15;
+    } else {
+        insResult.style.display = "none";
+        insVal = 0;
+    }
+}
+
+function checkOut() {
+    var totalCost = document.getElementById("totalCost");
+    rentTotal = parseInt(daysRented.value, 10) * 10 + insVal;
+    var userReceipt = "Your final cost totals at $" + rentTotal + ". You are now renting the " + carSelect + " for " + daysRented.value + " Days.";
     totalCost.innerHTML = userReceipt;
 }
+
+
+
 
 seatsDropDown();
 
